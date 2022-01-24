2d756b4f540be1a82bbaec97bd953813155d2a99
ifotn <rich@infrontofthenet.com>
2022-01-24 13:26:36 -0500

    l3/2 - save artist w/genre

--- artist-details.php
+++ artist-details.php
@@ -14,6 +14,25 @@
                     <label for="name" class="control-label col-2">Name:</label>
                     <input name="name" id="name" />
                 </fieldset>
+                <fieldset class="form-group">
+                    <label for="genreId" class="control-label col-2">Genre:</label>
+                    <select name="genreId" id="genreId">
+                        <?php
+                        $db = new PDO('mysql:host=127.0.0.1;dbname=phptunes', 'root', '');
+                        $sql = "SELECT * FROM genres";
+
+                        $cmd = $db->prepare($sql);
+                        $cmd->execute();
+                        $genres = $cmd->fetchAll();
+
+                        foreach ($genres as $genre) {
+                            echo '<option value="' . $genre['genreId'] . '">' . $genre['name'] . '</option>';
+                        }
+
+                        $db = null;
+                        ?>
+                    </select>
+                </fieldset>
                 <button class="btn btn-primary offset-2 mt-2">Save</button>
             </form>
         </main>      
--- save-artist.php
+++ save-artist.php
@@ -8,20 +8,22 @@
         <?php
         // get form input using the $_POST array and store in a local var (optional but helps simplify syntax)
         $name = $_POST['name'];
-        //echo "Artist Name: $name";
+        $genreId = $_POST['genreId'];
 
         // connect to the db using the PDO library w/5 vals: db type / server / dbname / username / password
         // PDO is the current PHP standard data access library, replacing mysqli
         $db = new PDO('mysql:host=127.0.0.1;dbname=phptunes', 'root', '');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // set the SQL INSERT command to add a new record to our artists table & set up a parameter for the name
-        $sql = "INSERT INTO artists (name) VALUES (:name)";
+        $sql = "INSERT INTO artists (name, genreId) VALUES (:name, :genreId)";
 
         // populate our SQL command with our form inputs
         // -> is the PHP operator like the . operator in Java or C#
         // we can't use object.method in PHP because the . is for concatenation (stupidly!)
         $cmd = $db->prepare($sql);
         $cmd->bindParam(':name', $name, PDO::PARAM_STR, 100);
+        $cmd->bindParam(':genreId', $genreId, PDO::PARAM_INT);
 
         // execute the save command
         $cmd->execute();
